cba0b7f1577b1eaaafef5c092b349754753f3bc5
LuTaOr <luistamayoortiz@gmail.com>
2024-07-10 14:13:29 +0200

    Update Funcion_intro_datos.py
    He quitado comentarios # que se me habían olvidado quitar y he cambiado el nombre de una variable list_dict a solo list, pues inicialmente iba a ser así.
    Me he dado cuenta durante la presentación.
    El resto del código se mantiene inalterado.

--- Parte_II_Modelo_BD/Funcion_intro_datos.py
+++ Parte_II_Modelo_BD/Funcion_intro_datos.py
@@ -5,19 +5,13 @@ def intro_datos(tabla, ruta, cursor, sep = ";"):
 
 
     df_csv = pd.read_csv(ruta, sep=";") # convierte el csv en un DataFrame
-    lista_dict = list(df_csv.keys()) # saca la info de los nombres de las columnas pasandolas a una lista
-    df_csv.set_index(lista_dict, inplace = True) # quitamos el indice implicito y dejamos solo las columnas, no hay columna indice
+    lista = list(df_csv.keys()) # saca la info de los nombres de las columnas pasandolas a una lista
+    df_csv.set_index(lista, inplace = True) # quitamos el indice implicito y dejamos solo las columnas, no hay columna indice
     columnas = tuple(df_csv.index.names) # pasa a tupla los nombres de las columnas para poder ser usado directamente en la query
 
     for i in range(0,len(df_csv.index)):  # recorre el bucle tantas veces como filas haya que introducir  
         
         query = f'''INSERT INTO {tabla} {columnas}
-                    VALUES {df_csv.index[i]}'''
+                    VALUES {df_csv.index[i]}''' # aqui se accede a cada una de las tuplas donde se almacenan los datos de cada una de las columnas
         
         cursor.execute(query)
-
-
-# ruta = "./CSVs_para_migrar_a_BD/ciudades.csv" # sera uno de los argumentos de la funcion
-# tabla = "ciudades" # sera uno de los argumentos de la funcion
-
-# intro_datos (tabla, ruta)
